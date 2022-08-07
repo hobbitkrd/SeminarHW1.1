c2a0c414aeb6505a2172351756b1c139315c1d86
hobbitKrd <za.css@yandex.ru>
2022-08-07 15:27:26 +0300

    Add project files.

---
+++ Seminar1HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите число:");
+string num1 = Console.ReadLine();
+Console.WriteLine("Введите число:");
+string num2 = Console.ReadLine();
+GetMax(num1,num2);
+
+
+
+
+
+int GetMax(string firstValue,string secondValue)
+{
+    if (firstValue != secondValue)
+    {
+        return firstValue > secondValue ? firstValue : secondValue;
+    }
+    else return firstValue;
+}
